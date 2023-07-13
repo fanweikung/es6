24eaf9abf7a9d6b9ddb8172b183c27ff18827d1f
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-07-13 05:54:58 -0700

    04_03 deconstruct with {}

--- Ch04/04_03/start/script.js
+++ Ch04/04_03/start/script.js
@@ -1,10 +1,9 @@
-const sandwich = {
+const { title, price } = {
   title: "Reuben",
   price: 7,
   description: "A classic",
   ingredients: ["bread", "corned beef", "dressing", "sauerkraut", "cheese"],
 };
 
-console.log(sandwich);
-console.log(sandwich.title);
-console.log(sandwich.price);
+console.log(title);
+console.log(price);
