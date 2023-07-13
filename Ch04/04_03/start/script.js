f07e11fd7c6204b743d7fe55ac57f7c8cb2ab88f
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-07-13 05:48:39 -0700

    04_03 added sandwich object

---
+++ Ch04/04_03/start/script.js
@@ -0,0 +1,10 @@
+const sandwich = {
+  title: "Reuben",
+  price: 7,
+  description: "A classic",
+  ingredients: ["bread", "corned beef", "dressing", "sauerkraut", "cheese"],
+};
+
+console.log(sandwich);
+console.log(sandwich.title);
+console.log(sandwich.price);
